d88975cce462e6312b57782bc36e50efa1b1b316
bszeidl <szeidl.balazs@hotmail.com>
2023-06-26 21:45:13 +0200

    Lesson completed, code runs.

---
+++ age_calculator.py
@@ -0,0 +1,5 @@
+import years
+
+def define_age(birth_year):
+    return (years.current_year - birth_year)
+
---
+++ main.py
@@ -0,0 +1,7 @@
+import age_calculator
+
+
+def age_printer(age):
+    print(str(age) + ' éves vagyok.')
+
+age_printer(age_calculator.define_age(1969))
